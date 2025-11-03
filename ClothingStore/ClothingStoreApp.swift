5bf447f58885872e27f2290c3e3052820dc79e19
ChandanH123 <chandanhayaran@gmail.com>
2025-11-03 11:48:17 +0530

    feature: initialized project structure with basic navigation.

---
+++ ClothingStore/ClothingStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingStoreApp.swift
+//  ClothingStore
+//
+//  Created by Chandan on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ClothingStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothingStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClothingStore
+//
+//  Created by Chandan on 03/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Clothing Store")
+                .navigationTitle("Products")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
